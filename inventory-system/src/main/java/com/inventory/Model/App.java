1f81f2fbded5b9aefb255c04378d5cde897424ac
Divyansh Shrivastava <divyanshshrivastava514@gmail.com>
2025-09-14 07:37:56 +0000

    Added folders and classes

---
+++ inventory-system/src/main/java/com/inventory/Model/App.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App{
+    public static void main(String[] args){
+        ArrayList<String> list=new ArrayList<>();
+
+        list.add("sugar");
+        list.add("chilly");
+
+        for(String t:list){
+            System.out.println(t);
+        }
+    }
+}
